125bb94a9f81c32ce16ad383ad307dc41a603f14
Tianyi <tianyi.song@rakuten.com>
2020-04-15 17:08:06 +0800

    Initial Commit

---
+++ CollectionViewController_SampleProject/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  CollectionViewController_SampleProject
+//
+//  Created by Song, Tianyi | RASIA on 15/4/20.
+//  Copyright © 2020 Song, Tianyi | RASIA. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
